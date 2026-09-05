9924ed753fbbcc6e22e918e113881091f651c685
abhishek kumar hansda <118197040+abhishekkrhansda@users.noreply.github.com>
2026-09-05 17:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+       h1{
+			color : #2a7ae2;
+		}
+
+		h2{
+			color : #f44336;
+		}
+
+		h3{
+			color : #4caf50;
+		}
+
+		h4{
+			color : #ff9800
+		}
+
+		h5{
+			color :  #607d8b
+		}
+
+		h6{
+			color :  #ffeb3b
+		}
